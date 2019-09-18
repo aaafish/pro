770f750a1930792f85baaa29b18816619bc5bf5a
aaafish <2179505465@qq.com>
2019-09-18 17:15:56 +0800

    响应式布局
    响应式布局

---
+++ xy-style/js/index.js
@@ -0,0 +1,33 @@
+(function(doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function() {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            if (clientWidth >= 640) {
+                docEl.style.fontSize = '100px';
+            } else {
+                docEl.style.fontSize = 100 * (clientWidth / 640) + 'px';
+            }
+        };
+
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+    recalc();
+})(document, window);
+
+
+var menuBtn = $(".header a");
+var menuList = $(".header nav");
+// console.log(menuList.css('height'));
+menuBtn.on("click",function(){
+    if(menuList.css('height')!='0px'){
+        menuList.css({'height':'0px'});
+        console.log(menuList.css('height'));
+    }
+    else if(menuList.css('height')=='0px'){
+        menuList.css({'height':'2rem'});
+        console.log(menuList.css('height'));
+    }
+});
